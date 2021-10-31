17a38c38c477700139c596aa1106e74eb8d1ee58
gomchi <lyn_94@naver.com>
2021-10-31 17:10:41 +0900

    Megabox Project

---
+++ assets/js/mySwiper.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
